146b8c7fbcc23a0ef50015d3cfcfc48743ca1214
Oksana Kysylenko <kisilenko.95@mail.ru>
2018-04-02 02:04:28 +0300

    site

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $('selector').loupe({
+        width: 200, // width of magnifier
+        height: 150, // height of magnifier
+        loupe: 'loupe' // css class for magnifier
+    });
+    $('.demo-2').loupe();
+});
