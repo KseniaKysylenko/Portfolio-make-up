53a8b1aaa181564bde5d9f32473d02c87266261f
Oksana Kysylenko <kisilenko.95@mail.ru>
2018-04-02 12:45:41 +0300

    optim.file

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-$(document).ready(function(){
-
-    $('selector').loupe({
-        width: 200, // width of magnifier
-        height: 150, // height of magnifier
-        loupe: 'loupe' // css class for magnifier
-    });
-    $('.demo-2').loupe();
-});
+$(document).ready(function(){$("selector").loupe({width:200,height:150,loupe:"loupe"}),$(".demo-2").loupe()});
